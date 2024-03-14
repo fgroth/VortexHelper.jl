d340024b18f4a1a54e8899a53850ee83a6ff9a95
Frederick Groth <fgroth@usm.lmu.de>
2024-03-14 15:43:27 +0100

    return dictionary for easier handling of data

--- src/read_vortex.jl
+++ src/read_vortex.jl
@@ -4,57 +4,55 @@ using FortranFiles
     read_particle_data(file::String, include_density::Bool=true)
 
 Read particle data from unformatted fortran output of vortex.
-Return `pos`, `vel`, `mass`, `vel_orig`, `velcomp`, `velrot`(, `rho`) vectors.
+Return `Dict` containing `"pos"`, `"vel_orig"`, `"mass"`, `"vel"`, `"velcomp"`, `"velrot"`(, `"rho"`) vectors.
 """
 function read_particle_data(file::String, include_density::Bool=true)
 
     f = FortranFile(file) 
     n = read(f,Int32)
 
+    data=Dict()
+    
     # read positions
-    pos = zeros(Float32,3,n)
-    pos[1,:] = read(f,pos[1,:])
-    pos[2,:] = read(f,pos[2,:])
-    pos[3,:] = read(f,pos[3,:])
+    data["pos"] = zeros(Float32,3,n)
+    data["pos"][1,:] = read(f,data["pos"][1,:])
+    data["pos"][2,:] = read(f,data["pos"][2,:])
+    data["pos"][3,:] = read(f,data["pos"][3,:])
 
     # read velocities
-    vel = zeros(Float32,3,n)
-    vel[1,:] = read(f,vel[1,:])
-    vel[2,:] = read(f,vel[2,:])
-    vel[3,:] = read(f,vel[3,:])
-
+    data["vel_orig"] = zeros(Float32,3,n)
+    data["vel_orig"][1,:] = read(f,data["vel_orig"][1,:])
+    data["vel_orig"][2,:] = read(f,data["vel_orig"][2,:])
+    data["vel_orig"][3,:] = read(f,data["vel_orig"][3,:])
+    
     # read mass
-    mass = zeros(Float32,n)
-    mass = read(f,mass)
-
+    data["mass"] = zeros(Float32,n)
+    data["mass"] = read(f,data["mass"])
+    
     # read velocities
-    vel_orig = zeros(Float32,3,n)
-    vel_orig[1,:] = read(f,vel[1,:])
-    vel_orig[2,:] = read(f,vel[2,:])
-    vel_orig[3,:] = read(f,vel[3,:])
+    data["vel"] = zeros(Float32,3,n)
+    data["vel"][1,:] = read(f,data["vel"][1,:])
+    data["vel"][2,:] = read(f,data["vel"][2,:])
+    data["vel"][3,:] = read(f,data["vel"][3,:])
     
     # read compressive velocities
-    velcomp = zeros(Float32,3,n)
-    velcomp[1,:] = read(f,velcomp[1,:])
-    velcomp[2,:] = read(f,velcomp[2,:])
-    velcomp[3,:] = read(f,velcomp[3,:])
+    data["velcomp"] = zeros(Float32,3,n)
+    data["velcomp"][1,:] = read(f,data["velcomp"][1,:])
+    data["velcomp"][2,:] = read(f,data["velcomp"][2,:])
+    data["velcomp"][3,:] = read(f,data["velcomp"][3,:])
 
     # read solenoidal velocities
-    velrot = zeros(Float32,3,n)    
-    velrot[1,:] = read(f,velrot[1,:])
-    velrot[2,:] = read(f,velrot[2,:])
-    velrot[3,:] = read(f,velrot[3,:])
+    data["velrot"] = zeros(Float32,3,n)    
+    data["velrot"][1,:] = read(f,data["velrot"][1,:])
+    data["velrot"][2,:] = read(f,data["velrot"][2,:])
+    data["velrot"][3,:] = read(f,data["velrot"][3,:])
 
     if include_density
         # read mass
-        rho = zeros(Float32,n)
-        rho = read(f,rho)
+        data["rho"] = zeros(Float32,n)
+        data["rho"] = read(f,data["rho"])
     end
 
-    if include_density
-        return pos, vel, mass, vel_orig, velcomp, velrot, rho
-    else
-        return pos, vel, mass, vel_orig, velcomp, velrot
-    end
-
+    return data
+    
 end
